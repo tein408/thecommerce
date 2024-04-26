21710c38c8858531f969504b00e8a3d04b5a33d8
Eunha Kang <75615404+tein408@users.noreply.github.com>
2024-04-26 12:39:48 +0900

    feat: add UserServiceTests

---
+++ user/src/test/java/com/thecommerce/user/UserServiceTests.java
@@ -0,0 +1,32 @@
+package com.thecommerce.user;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.thecommerce.user.user.UserRepository;
+import com.thecommerce.user.user.UserService;
+import com.thecommerce.user.user.userDTO.UserDTO;
+import com.thecommerce.user.user.UserRegistrationStatus;
+
+@SpringBootTest
+class UserServiceTests {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    void testSaveUser() {
+        UserDTO userDTO = new UserDTO(null, "test_user", "test@example.com", "password123", null);
+
+        UserRegistrationStatus result = userService.save(userDTO);
+        assertEquals(UserRegistrationStatus.OK, result);
+    }
+
+}
