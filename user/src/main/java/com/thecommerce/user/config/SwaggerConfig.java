9c5dd0abf454b248031badf516da46a9e22af06a
Eunha Kang <75615404+tein408@users.noreply.github.com>
2024-04-26 22:59:27 +0900

    chore: modify swagger doc

--- user/src/main/java/com/thecommerce/user/config/SwaggerConfig.java
+++ user/src/main/java/com/thecommerce/user/config/SwaggerConfig.java
@@ -10,6 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.Contact;
 
 @Configuration
 @EnableSwagger2
@@ -27,9 +28,10 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Your API Title")
-                .description("Your API Description")
+                .title("User API")
+                .description("회원 기능을 담당하는 API 입니다.")
                 .version("1.0.0")
+                .contact(new Contact("강은하", "https://www.linkedin.com/in/eunhakang/", "tein408@gmail.com"))
                 .build();
     }
 
