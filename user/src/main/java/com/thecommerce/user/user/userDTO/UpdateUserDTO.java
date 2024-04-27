bd216a1663908a991bb394a6cb2549261a9bb3d3
Eunha Kang <75615404+tein408@users.noreply.github.com>
2024-04-27 15:59:31 +0900

    refactor: modify to a more suitable annotation

--- user/src/main/java/com/thecommerce/user/user/userDTO/UpdateUserDTO.java
+++ user/src/main/java/com/thecommerce/user/user/userDTO/UpdateUserDTO.java
@@ -1,10 +1,12 @@
 package com.thecommerce.user.user.userDTO;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateUserDTO {
--- user/src/main/java/com/thecommerce/user/user/userDTO/UserDTO.java
+++ user/src/main/java/com/thecommerce/user/user/userDTO/UserDTO.java
@@ -1,12 +1,14 @@
 package com.thecommerce.user.user.userDTO;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
